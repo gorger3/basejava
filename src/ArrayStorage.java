acc97cfa740395249e63043ab9b945362bfd8ba7
Горелик Герман Александрович <GGorelik@vimpelcom.ru>
2022-12-07 13:59:02 +0300

    HW1: replace method "size()" with field "size"; use mehtod Arrays.copyOf; вwrite methods get, delete without creation of a new array

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,50 +1,55 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = size();
 
     void clear() {
-        for (Resume r : getAll()) {
-            r = null;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+            size = 0;
         }
     }
 
     void save(Resume r) {
-        int size = size();
         if (size < 10000) {
             storage[size] = r;
+            size = size + 1;
         } else {
             System.out.println("There is no place in the storage for a new resume. To save the resume delete any old one you don't need");
         }
     }
 
     Resume get(String uuid) {
-        Resume[] all = getAll();
-        for (Resume r : all) {
-            if (r.toString().equals(uuid)) return r;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) return storage[i];
         }
         System.out.println("There is no resume with uuid = " + uuid);
         return null;
     }
 
     void delete(String uuid) {
-        Resume[] all = getAll();
         int indexOfResumeToDelete = -1;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (indexOfResumeToDelete < 0) {
-                if (all[i].toString().equals(uuid)) {
+                if (storage[i].toString().equals(uuid)) {
                     indexOfResumeToDelete = i;
 
                 }
             } else {
-                all[i] = storage[i + 1];
+                if (i < size-1) { // Чтобы избежать переполнения массива в строке ниже
+                    storage[i] = storage[i + 1];
+                }
             }
         }
         if (indexOfResumeToDelete < 0) {
             System.out.println("There is no resume with uuid = " + uuid);
         } else {
-            all[size() - 1] = null;
+            storage[size - 1] = null;
+            size = size -1;
         }
     }
 
@@ -52,18 +57,14 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int i = 0;
-        while (storage[i] != null) {
-            i++;
-        }
-        Resume[] all = new Resume[i];
-        for (int j = 0; j < i; j++) {
-            all[j] = storage[j];
-        }
-        return all;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return getAll().length;
+        int i = 0;
+        while (storage[i] != null && i < 10000) {
+            i++;
+        }
+        return i;
     }
 }
