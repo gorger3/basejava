3603c4ed7f934fd777a8e7b876fab1e7e397845a
Горелик Герман Александрович <GGorelik@vimpelcom.ru>
2023-02-11 15:06:49 +0300

    HW10: xml

--- src/com/urise/webapp/storage/serializer/StreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/StreamSerializer.java
@@ -2,10 +2,11 @@ package com.urise.webapp.storage.serializer;
 
 import com.urise.webapp.model.Resume;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface StreamSerializer {
-    void doWrite(Resume r, OutputStream os);
-    Resume doRead(InputStream is);
+    void doWrite(Resume r, OutputStream os) throws IOException;
+    Resume doRead(InputStream is) throws IOException;
 }
