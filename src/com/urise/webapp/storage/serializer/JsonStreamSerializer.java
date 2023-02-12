58d94ddb738c8030ffcf09dd5a97734e4aae25d7
Горелик Герман Александрович <GGorelik@vimpelcom.ru>
2023-02-12 12:45:27 +0300

    HW10: json

---
+++ src/com/urise/webapp/storage/serializer/JsonStreamSerializer.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.storage.serializer;
+
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.util.JsonParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class JsonStreamSerializer implements StreamSerializer{
+
+    @Override
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
+        try(Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            JsonParser.write(resume, w);
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try(Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return JsonParser.read(r, Resume.class);
+        }
+    }
+}
