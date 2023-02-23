bab7c0ebe603a878b958fb4e3ce62186baa23520
Горелик Герман Александрович <GGorelik@vimpelcom.ru>
2023-02-23 10:19:49 +0300

    HW10: forth try — functional improvements to reading.

--- src/com/urise/webapp/storage/serializer/DataConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.urise.webapp.storage.serializer;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface DataConsumer<T> {
-    void writeData(T t) throws IOException;
-}
