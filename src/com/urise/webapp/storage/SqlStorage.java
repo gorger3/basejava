0b6e4490c7c986a8868b0653a6c62e984e736a7f
Горелик Герман Александрович <GGorelik@vimpelcom.ru>
2023-03-25 19:19:41 +0300

    HW14: join contacts (without transactions)

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.storage;
 
 import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.sql.SqlHelper;
 
@@ -8,7 +9,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SqlStorage implements Storage {
 
@@ -27,15 +31,68 @@ public class SqlStorage implements Storage {
 
     @Override
     public void update(Resume r) {
+        String uuid = r.getUuid();
         String query = "UPDATE resume SET full_name = ? WHERE uuid = ?";
         sqlHelper.executePreparedStatement(query, ps -> {
             ps.setString(1, r.getFullName());
-            ps.setString(2, r.getUuid());
+            ps.setString(2, uuid);
             if (ps.executeUpdate() == 0) {
                 throw new NotExistStorageException(r);
             }
             return null;
         });
+        if (r.getContacts().entrySet().size() == 0) { // если у обновлённого резюме нет контактов, удаляет старые контакты
+            String queryForContacts = "DELETE FROM contact WHERE resume_uuid = ?";
+            sqlHelper.executePreparedStatement(queryForContacts, ps -> {
+                ps.setString(1, uuid);
+                ps.executeUpdate();
+                return null;
+            });
+        } else { // если у обновлённого резюме есть контакты
+            Map<ContactType, String> mapFromResume = r.getContacts();
+            String queryToGetExistingContacts = "SELECT * FROM contact WHERE resume_uuid = ?"; // достаёт старые контакты из таблицы
+            List<ContactType> listOfUpdatedContacts = new ArrayList<>();
+            sqlHelper.executePreparedStatement(queryToGetExistingContacts, ps -> {
+                ps.setString(1, uuid);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    ContactType type = ContactType.valueOf(rs.getString("type"));
+                    if (mapFromResume.containsKey(type)) { // если контакту в обновлённом резюме есть соответствие в таблице старых контактов
+                        String queryToUpdateContact = "UPDATE mydb.public.contact SET value = ? WHERE type = ? AND resume_uuid = ?";
+                        sqlHelper.executePreparedStatement(queryToUpdateContact, psU -> {
+                            psU.setString(1, mapFromResume.get(type));
+                            psU.setString(2, type.name());
+                            psU.setString(3, uuid);
+                            psU.execute();
+                            return null;
+                        });
+                        listOfUpdatedContacts.add(type);
+                    } else { // если контакту в обновлённом резюме нет соответствие в таблице старых контактов
+                        String queryToDeleteContact = "DELETE FROM mydb.public.contact WHERE type = ? AND resume_uuid = ?";
+                        sqlHelper.executePreparedStatement(queryToDeleteContact, psD -> {
+                            psD.setString(1, type.name());
+                            psD.setString(2, uuid);
+                            psD.execute();
+                            return null;
+                        });
+                    }
+                }
+                return null;
+            });
+            for (Map.Entry<ContactType, String> entry : r.getContacts().entrySet()) { // сохраняет контакты из обновлённого резюме, которых нет в таблице старых контактов
+                if (listOfUpdatedContacts.contains(entry.getKey())) { // если контакт из резюме есть в списке контактов, уже обновлённых в таблице
+                    continue; // начинает новую итерацию, поскольку контакт уже был обновлён с помощью UPDATE
+                }
+                String queryToSaveNewContact = "INSERT INTO mydb.public.contact (resume_uuid, type, value) VALUES (?, ?, ?)"; // сохраняет новые контакты из обновлённого резюме
+                sqlHelper.executePreparedStatement(queryToSaveNewContact, psS -> {
+                    psS.setString(1, uuid);
+                    psS.setString(2, entry.getKey().name());
+                    psS.setString(3, entry.getValue());
+                    psS.execute();
+                    return null;
+                });
+            }
+        }
     }
 
     @Override
@@ -47,18 +104,40 @@ public class SqlStorage implements Storage {
             ps.execute();
             return null;
         });
+        String queryForContacts = "INSERT INTO contact (resume_uuid, type, value) VALUES (?, ?, ?)";
+        for (Map.Entry<ContactType, String> e : r.getContacts().entrySet()) {
+            sqlHelper.executePreparedStatement(queryForContacts, ps -> {
+                ps.setString(1, r.getUuid());
+                ps.setString(2, e.getKey().name());
+                ps.setString(3, e.getValue());
+                ps.execute();
+                return null;
+            });
+        }
     }
 
     @Override
     public Resume get(String uuid) {
-        String query = "SELECT * FROM resume r WHERE r.uuid = ?";
+        String query = "SELECT * FROM resume r " +
+                "LEFT JOIN contact c " +
+                "ON r.uuid = c.resume_uuid " +
+                "WHERE r.uuid = ?";
         return sqlHelper.executePreparedStatement(query, ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
-            return new Resume(uuid, rs.getString("full_name"));
+            Resume r = new Resume(uuid, rs.getString("full_name"));
+            do {
+                String type = rs.getString("type");
+                if (type != null) {
+                    String value = rs.getString("value");
+                    ContactType contactType = ContactType.valueOf(type);
+                    r.addContact(contactType, value);
+                }
+            } while (rs.next());
+            return r;
         });
 
     }
@@ -77,15 +156,30 @@ public class SqlStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
-        String query = "SELECT * FROM resume ORDER BY full_name, uuid";
+        String query = "SELECT * FROM resume r " +
+                "LEFT JOIN contact c " +
+                "ON r.uuid = c.resume_uuid " +
+                "ORDER BY r.full_name, r.uuid";
         return sqlHelper.executePreparedStatement(query, ps -> {
             ResultSet rs = ps.executeQuery();
-            List<Resume> list = new ArrayList<>();
+            Map<String, Resume> map = new HashMap<>();
             while (rs.next()) {
-                list.add(new Resume((rs.getString("uuid")),
-                        (rs.getString("full_name"))));
+                String uuidFromTable = rs.getString("uuid");
+                Resume r;
+                if (map.containsKey(uuidFromTable)) {
+                    r = map.get(uuidFromTable);
+                } else {
+                    r = new Resume((rs.getString("uuid")), (rs.getString("full_name")));
+                    map.put(uuidFromTable, r);
+                }
+                String type = rs.getString("type");
+                if (type != null) {
+                    String value = rs.getString("value");
+                    ContactType contactType = ContactType.valueOf(type);
+                    r.addContact(contactType, value);
+                }
             }
-            return list;
+            return map.values().stream().sorted().collect(Collectors.toList());
         });
     }
 
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -3,6 +3,7 @@ package com.urise.webapp.storage;
 import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,8 +37,8 @@ public abstract class AbstractStorageTest {
         R3 = new Resume(UUID_3, "Name3");
         R4 = new Resume(UUID_4, "Name4");
 
-//        R1.addContact(ContactType.MAIL, "mail1@ya.ru");
-//        R1.addContact(ContactType.PHONE, "11111");
+        R1.addContact(ContactType.MAIL, "mail1@ya.ru");
+        R1.addContact(ContactType.PHONE, "11111");
 //        R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
 //        R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
 //        R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achievement11", "Achievement12", "Achievement13"));
